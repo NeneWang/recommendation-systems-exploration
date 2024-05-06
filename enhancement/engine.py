ba1c8a0e92c38f23e55749307a2196cee88d8783
NelsonWang <wangnelson2@gmail.com>
2024-05-06 16:19:25 -0400

    engine | recs

--- enhancement/engine.py
+++ enhancement/engine.py
@@ -8,12 +8,14 @@ import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 from gensim.models import Word2Vec
 import string
+import re
+
 
 product_data = {
     "data_context": "books",
     "product_filepath": "data/products_books_v1_10_10.csv",
     "transactions_filepath": "data/transactions_books_v1_10_10.csv",
-    "features": ["product_title", "product_image", "product_soup", "product_images"],
+    "features": ["id", "product_title", "product_image", "product_soup", "product_images"],
     "version": "1.0",
     "unique_name": "_books_v1_10_10",
 }
@@ -111,7 +113,7 @@ class CosineSimilarityRecommender(RecommendationAbstract):
     def train(self, transactions, auto_save=True):
         self.pt = transactions.pivot_table(index="product_id", columns="user_id", values="rate")
         self.pt.fillna(0, inplace=True)
-        self.sim_score = cosine_similarity(pt)
+        self.sim_score = cosine_similarity(self.pt)
         if auto_save:
             self.save()
         
@@ -147,6 +149,8 @@ class CosineSimilarityRecommender(RecommendationAbstract):
         return rec[:n]
 
 
+
+
 class WordVecBodyRecommender(RecommendationAbstract):
     
     strategy_name: str = "WordVec"
@@ -164,30 +168,38 @@ class WordVecBodyRecommender(RecommendationAbstract):
         super().__init__(products, product_data)
         self.products_df = products
         self.model = None
+        print('id_to_products length', len(self.id_to_products))
         self.train()
 
-    def train(self, save_model=False):
-        """
-        Train the Word2Vec model on the book descriptions.
-        """
-        self.model = Word2Vec(sentences=self.products['product_soup'].str.split(), vector_size=100, window=5, min_count=1, workers=4)
-        if save_model:
-            self.save()
-        
 
-        # Updated function to recommend books based on similar word vectors
+    def train(self, auto_save=False):
+        """
+        Train the Word2Vec model on the book titles.
+        """
+        sentences = [title.lower().split() for title in self.products_df['product_soup']]
+        self.model = Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=1, workers=4)
+        if auto_save:
+            self.save()
+            
+
     def recommend_books_updated(self, input_text, top_n=5):
-        input_text = input_text.lower().translate(str.maketrans('', '', string.punctuation)).split()
+        """
+        Return recommendations based on input text.
+        """
+        input_text = input_text.lower().split()
         vector = self.model.wv[input_text].mean(axis=0)
-        similar_vectors = self.model.wv.similar_by_vector(vector, topn=top_n + 10)  # Retrieve more results to filter unique titles
-        recommended_titles = []
-        for book_vector in similar_vectors:
-            similar_title = self.products.loc[self.products['product_soup'].apply(lambda x: any(word in x for word in input_text)), 'product_title'].unique()
-            for title in similar_title:
-                if title not in recommended_titles and len(recommended_titles) < top_n:
-                    recommended_titles.append(title)
-        return recommended_titles
-      
+        # Compute cosine similarity between the input vector and all product vectors
+        similarities = cosine_similarity([vector], self.model.wv.vectors)
+        # Get indices of top similar products
+        top_indices = similarities.argsort()[0][-top_n:]
+        recommended_products = []
+        for index in reversed(top_indices):  # Reversed to get top similarities first
+            product_title = self.products_df.iloc[index]['product_title']
+            confidence = similarities[0][index]
+            recommended_products.append((self.id_to_productDetail(self.products_df.iloc[index]['id']), confidence))
+        return recommended_products
+
+        
     def recommend_from_single(self, product_id, n=5):
         """
         Return recommendations based on a single product.
@@ -197,7 +209,7 @@ class WordVecBodyRecommender(RecommendationAbstract):
         # Use the recommend_books_updated function to recommend books based on the product_soup
         recommendations = self.recommend_books_updated(product_soup, top_n=n)
         return recommendations
-    
+
     def recommend_from_past(self, transactions, n=10):
         """
         Return recommendations based on past transactions.
@@ -211,7 +223,7 @@ class WordVecBodyRecommender(RecommendationAbstract):
         
     def get_filename(self):
         return  self.slug_name + self.product_data["unique_name"] + ".model"
-    
+
     def save(self):
         """
         Save the computed book vectors to a file.
@@ -232,7 +244,93 @@ class WordVecBodyRecommender(RecommendationAbstract):
         filemodel.close()
 
 
+class TitleWordVecTitleyRecommender(RecommendationAbstract):
+    
+    strategy_name: str = "TitleWordVec"
+    slug_name: str = "title_word_vec"
+    version: str = "v1"
+    details: str = "REQUIRES IMPLEMENTATION"
+    link: str = "REQUIRES IMPLEMENTATION"
+    supports_single_recommendation: bool = True
+    supports_past_recommendation: bool = True
+    
+    def __init__(self, products, product_data):
+        """
+        Initialize the recommender with a pre-trained Word2Vec model and a dataframe of books.
+        """
+        super().__init__(products, product_data)
+        self.products_df = products
+        self.model = None
+        self.train()
 
+    def train(self, auto_save=False):
+        """
+        Train the Word2Vec model on the book titles.
+        """
+        sentences = [title.lower().split() for title in self.products_df['product_title']]
+        self.model = Word2Vec(sentences=sentences, vector_size=100, window=5, min_count=1, workers=4)
+        if auto_save:
+            self.save()
 
+    def recommend_from_single(self, product_id, n=5):
+        """
+        Return recommendations based on a single product.
+        """
+        # Get the product_title for the given product_id
+        product_title = self.products_df.loc[self.products_df['id'] == product_id, 'product_title'].values[0]
+        # Use the recommend_books_updated function to recommend books based on the product_title
+        recommendations = self.recommend_books_updated(product_title, top_n=n)
+        return recommendations
+    
+    def recommend_from_past(self, transactions, n=10):
+        """
+        Return recommendations based on past transactions.
+        """
+        # Concatenate product_titles from past transactions
+        past_titles = self.products_df.loc[self.products_df['id'].isin(transactions), 'product_title']
+        # Use the self.recommend_books_updated function to recommend books based on past transactions
+        recommendations = self.recommend_books_updated(' '.join(past_titles), top_n=n)
+        return recommendations
+
+        
+
+    def recommend_books_updated(self, input_text, top_n=5):
+        """
+        Return recommendations based on input text.
+        """
+        input_text = input_text.lower().split()
+        vector = self.model.wv[input_text].mean(axis=0)
+        # Compute cosine similarity between the input vector and all product vectors
+        similarities = cosine_similarity([vector], self.model.wv.vectors)
+        # Get indices of top similar products
+        top_indices = similarities.argsort()[0][-top_n:]
+        recommended_products = []
+        for index in reversed(top_indices):  # Reversed to get top similarities first
+            product_title = self.products_df.iloc[index]['product_title']
+            confidence = similarities[0][index]
+            recommended_products.append((self.id_to_productDetail(self.products_df.iloc[index]['id']), confidence))
+        return recommended_products
+
+    def save(self):
+        """
+        Save the computed book vectors to a file.
+        """
+        filename = self.get_filename()
+        with open(filename, 'wb') as filemodel:
+            pickle.dump(self.model, filemodel)
+
+    def load(self):
+        """
+        Load the book vectors from a file.
+        """
+        filename = self.get_filename()
+        with open(filename, 'rb') as filemodel:
+            self.model = pickle.load(filemodel)
+
+    def get_filename(self):
+        """
+        Get the filename for saving/loading the model.
+        """
+        return self.slug_name + self.product_data["unique_name"] + ".model"
 
 
