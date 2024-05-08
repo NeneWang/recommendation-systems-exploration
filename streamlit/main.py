c23361e06e94ce13b417fb6d38e1ea367e1be99e
NelsonWang <wangnelson2@gmail.com>
2024-05-08 12:23:01 -0400

    rec

--- streamlit/main.py
+++ streamlit/main.py
@@ -44,12 +44,41 @@ engines = {
         "engine": wordVecBodyRecommender
     },
     "wordvec_title": {
+        "title": "WordVec Title",
         "engine": titleWordVecTitleRecommender
     },
     "word_vec_title_v2": {
+        "title": "WordVec Title V2",
         "engine": titleWordVectRecommender2
     
-    }
+    },
+    "collaborative_filtering": {
+        "title": "Collaborative Filtering",
+        "engine": cosineSimilarRecommender
+    },
+    "KNN Basic": {
+        "title": "KNN Basic",
+        "engine": cosineSimilarRecommender
+    },
+    
+    "matrix_factorization SVD": {
+        "title": "Base Algorithm",
+        "engine": cosineSimilarRecommender
+    },
+    "matrix_factorization SVD++": {
+        "title": "Base Algorithm",
+        "engine": cosineSimilarRecommender
+    },
+    "matrix_factorization KNNWithMeans": {
+        "title": "Base KNNWithMeans",
+        "engine": cosineSimilarRecommender
+    },
+    
+    "matrix_factorization KNNWithZscore": {
+        "title": "Base KNNWithMeans",
+        "engine": cosineSimilarRecommender
+    },
+    
 }
 
 # Define the available product cases
@@ -74,78 +103,79 @@ def get_product_id(product_name):
     return products_df.loc[products_df["product_title"] == product_name, "id"].iloc[0]
 
 
-# Streamlit app
-def main():
-    # Initialize session state
+
+
+if "selected_product" not in st.session_state:
+    st.session_state.selected_product = []
     
-    # user transactions
-    transactions_df = pd.DataFrame(columns=["id", "user_id", "product_id", "rate"])
+if "transactions_list" not in st.session_state:
+    st.session_state.transactions_list = []
+
+
+st.title("Product Recommendation Demo")
+
+recommend_single_engine = st.selectbox("Select the recommendation engine for single product:", list(engines.keys()))
+recommend_past_engine = st.selectbox("Select the recommendation engine for past transactions:", list(engines.keys()))
+
+# Select product type
+products_type = "books"  # Support more types in the future
+
+# Select base case
+base_case = st.selectbox("Select base case:", base_cases(products_type))
+if base_case != "empty_case":
+    st.session_state.selected_product = PRODUCT_CASES[products_type][base_case]
+
+st.write("Selected products:", st.session_state.selected_product)
+
+# Display products
+# st.write("Product Information:")
+# st.write(products_df)
+
+# Search and select products
+selected_product = st.selectbox("Select products to add to cart:", products_df["product_title"])
+st.session_state.selected_product = selected_product
+print(st.session_state.selected_product)
+# Buy button
+if st.button("Buy"):
+    transactions = st.session_state.transactions_list
+    product = st.session_state.selected_product
+    product_id = get_product_id(product)
+    transaction = {
+        "id": product_id,
+        "user_id": 1,
+        "product_id": product_id,
+        'product_title': product, 
+        "rate": 5
+    }
+    transactions.append(transaction)
+    st.session_state.transactions_list = transactions
     
-    if "selected_product" not in st.session_state:
-        st.session_state.selected_product = []
 
-    st.title("Product Recommendation Demo")
-
-    recommend_single_engine = st.selectbox("Select the recommendation engine for single product:", list(engines.keys()))
-    recommend_past_engine = st.selectbox("Select the recommendation engine for past transactions:", list(engines.keys()))
-
-    # Select product type
-    products_type = "books"  # Support more types in the future
-
-    # Select base case
-    base_case = st.selectbox("Select base case:", base_cases(products_type))
-    if base_case != "empty_case":
-        st.session_state.selected_product = PRODUCT_CASES[products_type][base_case]
-
-    st.write("Selected products:", st.session_state.selected_product)
-
-    # Display products
-    # st.write("Product Information:")
-    # st.write(products_df)
-
-    # Search and select products
-    selected_product = st.selectbox("Select products to add to cart:", products_df["product_title"])
-    st.session_state.selected_product = selected_product
-    print(st.session_state.selected_product)
-    # Buy button
-    if st.button("Buy"):
-        transactions = []
+# Get recommendations
+if st.button("Get Recommendations"):
+    if st.session_state.selected_product:
+        
+        selected_engine: RecommendationAbstract = engines[recommend_single_engine]["engine"]
         product = st.session_state.selected_product
+        selected_engine.load()
         product_id = get_product_id(product)
-        transaction = {
-            "id": len(transactions_df) + 1,
-            "user_id": 1,
-            "product_id": product_id,
-            'product_title': product, 
-            "rate": 1
-        }
-        transactions.append(transaction)
-        transactions_df = pd.concat([transactions_df, pd.DataFrame(transactions)], ignore_index=True)
-        st.write("Transaction:", transaction)
+        # recommend_single = engines[recommend_single_engine].recommend_from_past(st.session_state.selected_product)
+        # selected_engine = 
+        recommendation = selected_engine.recommend_from_single(product_id=product_id)
+        st.write("Recommendations based on selected products:", recommendation)
+    else:
+        st.warning("Please select some products first!")
 
-    # Get recommendations
-    if st.button("Get Recommendations"):
-        if st.session_state.selected_product:
-            
-            selected_engine: RecommendationAbstract = engines[recommend_single_engine]["engine"]
-            product = st.session_state.selected_product
-            selected_engine.load()
-            product_id = get_product_id(product)
-            # recommend_single = engines[recommend_single_engine].recommend_from_past(st.session_state.selected_product)
-            # selected_engine = 
-            recommendation = selected_engine.recommend_from_single(product_id=product_id)
-            st.write("Recommendations based on selected products:", recommendation)
-        else:
-            st.warning("Please select some products first!")
+    if not st.session_state.transaction_list.empty:
+        # last_transaction = transactions_df.iloc[-1]
+        # recommend_past = engines[recommend_past_engine].recommend_from_past([last_transaction])
+        # st.write("Recommendations based on past transaction:", recommend_past)
+        recommend_past = engines[recommend_past_engine]["engine"].recommend_from_past(st.session_state.transactions_list)
+        st.write("Recommendations based on past transaction:", recommend_past)
 
-        if not transactions_df.empty:
-            last_transaction = transactions_df.iloc[-1]
-            recommend_past = engines[recommend_past_engine].recommend_from_past([last_transaction])
-            st.write("Recommendations based on past transaction:", recommend_past)
-
-    # Show transactions
-    st.write("Transaction History:")
-    st.write(transactions_df)
-
-if __name__ == "__main__":
-    main()
+# Show transactions
+st.write("Transaction History:")
+# Show st.session_state.transactions_list
+transactions_df = pd.DataFrame(st.session_state.transactions_list)
+st.write(transactions_df)
+    
